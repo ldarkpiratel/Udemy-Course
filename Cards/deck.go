d6029970c5d0971ff59c997ddf5396c77a3416cd
vTalio <40224197+vTalio@users.noreply.github.com>
2020-12-29 14:27:23 -0400

    Add files via upload

--- Cards/deck.go
+++ Cards/deck.go
@@ -1,6 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"os"
+	"strings"
+	"time"
+)
 
 // Create a new type of 'deck'
 // which is a slice of strings
@@ -9,7 +16,7 @@ type deck []string
 func newDeck() deck {
 	cards := deck{}
 	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
-	cardValues := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
 
 	for _, suit := range cardSuits {
 		for _, value := range cardValues {
@@ -24,5 +31,56 @@ func newDeck() deck {
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
+	}
+}
+
+// take first argument of type deck, and a second argument of type int
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+}
+
+// receiver function
+func (d deck) toString() string {
+	// convert slice type deck to type string then join every element in a slice into 1 string
+	// the strings.Join() function receives the slice as the first argument, then the separation
+	// character after joining the slice
+	return strings.Join([]string(d), ", ")
+}
+
+func (d deck) saveToFile(filename string) error {
+	// ioutil.WriteFile() requires 3 arguments: the name of the file to create, the byte slice and the permissions for the file
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+func newDeckFromFile(filename string) deck {
+	bs, err := ioutil.ReadFile(filename)
+	if err != nil {
+		// option #1 - log the error and return a call to newDeck()
+		// option #2 - log the error and quit program
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+
+	s := strings.Split(string(bs), ", ")
+	return deck(s)
+
+}
+
+func (d deck) shuffle() {
+
+	// generate new source (seed) value for random numbers to be generated
+	// use time package to use type Int64 as argument to the function
+	source := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(source)
+
+	for i := range d {
+		// pseudorandom number generator
+		// get random index from deck slice
+		newPosition := r.Intn(len(d) - 1)
+
+		// shuffle elements on slice to new positions using random index generated
+		// this swaps elements in the slice
+		d[i], d[newPosition] = d[newPosition], d[i]
+
 	}
 }
--- Cards/main.go
+++ Cards/main.go
@@ -2,7 +2,7 @@ package main
 
 func main() {
 
-	card := newDeck()
+	//card := newDeck()
 	//card := deck{"Ace of Diamonds", newCard()} // create a slice of type string using customized type deck
 	//card = append(card, "Six of Spades")       // add a new string to the slice
 
@@ -10,7 +10,29 @@ func main() {
 	//for i, cards := range card { // loop over slice card, and print the index and its element
 	//	fmt.Println(i+1, cards)
 	//}
-	card.print()
+	//card.print()
+	//hand, remainingCards := deal(card, 5)
+
+	//hand.print()
+	//remainingCards.print()
+
+	//greeting := "Hi there!"
+
+	// convert string to byte slice
+	//fmt.Println([]byte(greeting))
+
+	//cards := newDeck()
+	//fmt.Println(cards.toString())
+
+	// cards := newDeck()
+	// cards.saveToFile("my_cards")
+
+	//cards := newDeckFromFile("my_cards")
+	//cards.print()
+
+	cards := newDeck()
+	cards.shuffle()
+	cards.print()
 }
 
 // explicitly state data type string, to return a string (fits for other data types)
